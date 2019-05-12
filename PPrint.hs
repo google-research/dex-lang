beb5a1215af2dfa342a42f59085610ba6ba56558
Alexey Radul <axch@google.com>
2019-05-12 15:45:33 -0400

    Messing with the pretty-printer a little to get more line breaks and nicer output.

--- PPrint.hs
+++ PPrint.hs
@@ -77,10 +77,10 @@ instance Pretty Expr where
     Lit val      -> p val
     Var v        -> p v
     Builtin b    -> p b
-    Let pat e1 e2  -> align $ parens $ "let" <+> pPat pat <+> "=" <+> p e1 <> line <>
+    Let pat e1 e2  -> parens $ align $ "let" <+> (group $ align $ pPat pat <+> "=" <> line <> p e1) <> line <>
                                        "in" <+> p e2
-    Lam pat e    -> parens $ "lam" <+> pPat pat <+> ":" <+> align (p e)
-    App e1 e2    -> parens $ p e1 <+> p e2
+    Lam pat e    -> parens $ align $ group $ "lam" <+> pPat pat <+> ":" <> line <> align (p e)
+    App e1 e2    -> parens $ align $ group $ p e1 <> line <> p e2
     For t e      -> parens $ "for " <+> p (binder t) <+> ":" <+> align (p e)
     Get e ie     -> p e <> "." <> p ie
     RecCon r     -> p r
