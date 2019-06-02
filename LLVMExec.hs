9c5a41f1a8380bc8a4bd276aedb2babc652aede8
Dougal Maclaurin <dougalm@google.com>
2019-06-01 21:58:33 -0400

    Actually invoking some LLVM passes (oops..) speeds up matmul ~6X

--- LLVMExec.hs
+++ LLVMExec.hs
@@ -5,6 +5,7 @@ module LLVMExec (showLLVM, evalJit, readPtrs, wordAsPtr, ptrAsWord,
 
 import qualified LLVM.AST as L
 import qualified LLVM.Module as Mod
+import qualified LLVM.PassManager as P
 import qualified LLVM.ExecutionEngine as EE
 import LLVM.Internal.Context
 
@@ -23,12 +24,14 @@ evalJit :: L.Module -> IO ()
 evalJit mod =
   withContext $ \c ->
     Mod.withModuleFromAST c mod $ \m -> do
-      jit c $ \ee ->
-        EE.withModuleInEngine ee m $ \eee -> do
-          fn <- EE.getFunction eee (L.Name "thefun")
-          case fn of
-            Just fn -> runJitted fn
-            Nothing -> error "Failed to fetch \"thefun\" from LLVM"
+      P.withPassManager passes $ \pm -> do
+        P.runPassManager pm m
+        jit c $ \ee ->
+         EE.withModuleInEngine ee m $ \eee -> do
+           fn <- EE.getFunction eee (L.Name "thefun")
+           case fn of
+             Just fn -> runJitted fn
+             Nothing -> error "Failed to fetch \"thefun\" from LLVM"
   where
     jit :: Context -> (EE.MCJIT -> IO a) -> IO a
     jit c = EE.withMCJIT c (Just 3) Nothing Nothing Nothing
@@ -39,7 +42,9 @@ runJitted fn = haskFun (castFunPtr fn :: FunPtr (IO ()))
 showLLVM :: L.Module -> IO String
 showLLVM m = withContext $ \c ->
                Mod.withModuleFromAST c m $ \m ->
-                 fmap unpack $ Mod.moduleLLVMAssembly m
+                 P.withPassManager passes $ \pm -> do
+                   P.runPassManager pm m
+                   fmap unpack $ Mod.moduleLLVMAssembly m
 
 readPtrs :: Int -> Ptr Word64 -> IO [Word64]
 readPtrs n ptr = mapM readAt [0..n-1]
@@ -51,17 +56,5 @@ wordAsPtr x = wordPtrToPtr $ fromIntegral x
 ptrAsWord :: Ptr a -> Word64
 ptrAsWord ptr = fromIntegral $ ptrToWordPtr ptr
 
--- createPersistVal :: [CompileVal] -> Ptr () -> IO [PersistVal]
--- createPersistVal v ptr = undefined -- do
---   ptrRef <- newIORef ptr
---   traverse (getNext ptrRef) v
---   where
---     getNext :: IORef (Ptr ()) -> Operand -> IO PWord
---     getNext ref op = do
---       ptr <- readIORef ref
---       val <- peek (castPtr ptr :: Ptr Word64)
---       let b = opBaseType op
---       writeIORef ref (plusPtr ptr 8)
---       return $ if opIsPtr op
---                     then PPtr    b (wordPtrToPtr (fromIntegral val))
---                     else PScalar b val
+passes :: P.PassSetSpec
+passes = P.defaultCuratedPassSetSpec {P.optLevel = Just 3}
