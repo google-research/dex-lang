60a1895d7e3c6bea6b27a6df61ab264d21a13cb3
Dougal Maclaurin <dougalm@google.com>
2019-12-04 23:46:06 -0500

    Remove old quickcheck stuff since we've moved to hedgehog.

--- tests/PropTests.hs
+++ tests/PropTests.hs
@@ -6,60 +6,20 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-
-import Test.QuickCheck
-import Test.QuickCheck.Random
-import System.Exit
-import Data.Text.Prettyprint.Doc
 import Control.Monad
 import qualified Hedgehog as H
 import Control.Monad.Reader
 import GHC.Stack
 import qualified Data.Map.Strict as M
 
-
 import Syntax hiding (Result)
 import Parser
 import PPrint
-import Generators ()
 import GenExpr
 import TestPass
 
-prop_print_parse_uexpr :: UTopDecl -> Property
-prop_print_parse_uexpr decl = case parseTopDecl (pprintEsc decl) of
-  Left e -> counterexample (pprint e) False
-  Right decl' -> decl === stripSrcAnnotTopDecl decl'
-
--- wrapper to make show pretty
-data PPWrap a = PPWrap a  deriving (Eq)
-
-instance Pretty a => Show (PPWrap a) where
-  show (PPWrap x) = pprintEsc x
-
-instance Arbitrary a => Arbitrary (PPWrap a) where
-  arbitrary = liftM PPWrap arbitrary
-  shrink (PPWrap x) = map PPWrap (shrink x)
-
-fromPPWrap :: PPWrap a -> a
-fromPPWrap (PPWrap x) = x
-
-pprintProp :: (Pretty a, Arbitrary a, Testable prop) => (a -> prop) -> Property
-pprintProp f = property (f . fromPPWrap)
-
-args :: Args
-args = stdArgs
-  { maxSize = 100
-  , maxSuccess = 100
-  , replay = Just (mkQCGen 0, 0)
-  }
-
 main :: IO ()
-main = do
-  results <- quickCheckWithResult args (pprintProp prop_print_parse_uexpr)
-  _ <- tests
-  if isSuccess results
-    then return ()
-    else exitWith (ExitFailure 1)
+main = void tests
 
 evalIOEither :: (H.MonadTest m, Show x, MonadIO m, HasCallStack) => IO (Either x a) ->  m a
 evalIOEither m = H.evalIO m >>= H.evalEither
