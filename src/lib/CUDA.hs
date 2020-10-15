4de67e508c19621e594246b9c461ea410af7abe4
Adam Paszke <apaszke@google.com>
2020-10-15 09:13:58 +0000

    Fix the GPU backend

--- src/lib/CUDA.hs
+++ src/lib/CUDA.hs
@@ -7,15 +7,15 @@ import Foreign.Ptr
 hasCUDA :: Bool
 
 #ifdef DEX_CUDA
-foreign import ccall "load_cuda_array"  loadCUDAArrayImpl :: Ptr () -> Int64 -> IO (Ptr ())
+foreign import ccall "load_cuda_array"  loadCUDAArrayImpl :: Ptr () -> Ptr () -> Int64 -> IO ()
 
 hasCUDA = True
 #else
-loadCUDAArrayImpl :: Ptr () -> Int64 -> IO (Ptr ())
+loadCUDAArrayImpl :: Ptr () -> Ptr () -> Int64 -> IO ()
 loadCUDAArrayImpl _ _ = error "Dex built without CUDA support"
 
 hasCUDA = False
 #endif
 
-loadCUDAArray :: Ptr () -> Int -> IO (Ptr ())
-loadCUDAArray ptr bytes = loadCUDAArrayImpl ptr (fromIntegral bytes)
+loadCUDAArray :: Ptr () -> Ptr () -> Int -> IO ()
+loadCUDAArray hostPtr devicePtr bytes = loadCUDAArrayImpl hostPtr devicePtr (fromIntegral bytes)
