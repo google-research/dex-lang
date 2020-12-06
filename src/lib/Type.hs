71369201003389c6790e23f38ecc5b78ba03319c
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-12-06 17:34:46 +0000

    add codepount, Eq and Ord for Char

--- src/lib/Type.hs
+++ src/lib/Type.hs
@@ -728,6 +728,9 @@ typeCheckOp op = case op of
     i |: TC (IntRange (IdxRepVal 0) (IdxRepVal $ fromIntegral vectorWidth))
     return $ BaseTy $ Scalar sb
   ThrowError ty -> ty|:TyKind $> ty
+  -- TODO: type check that c is a character
+  -- TODO: this should really be a 32 bit integer for unicode reasons: 8 bit is just 1 codeunit
+  CodePoint c -> return $ BaseTy $ Scalar Int8Type
   CastOp t@(Var _) _ -> t |: TyKind $> t
   CastOp destTy e -> do
     sourceTy <- typeCheck e
