f9dd5c314495ba4d7ad9b9ec772c11110096df68
DHHZ <dhhzheng@google.com>
2021-06-21 13:55:10 +0100

    Add vmap to JAX interop.

--- python/tests/jax_test.py
+++ python/tests/jax_test.py
@@ -13,7 +13,9 @@ import jax.numpy as jnp
 import dex
 from dex.interop.jax import primitive
 
+
 class JAXTest(unittest.TestCase):
+
   def test_impl_scalar(self):
     add_two = primitive(dex.eval(r'\x:Float. x + 2.0'))
     x = jnp.zeros((), dtype=np.float32)
@@ -25,7 +27,8 @@ class JAXTest(unittest.TestCase):
     np.testing.assert_allclose(add_two(x), x + 2.0)
 
   def test_abstract_eval_simple(self):
-    add_two = primitive(dex.eval(r'\x:((Fin 10)=>Float). for i. FToI $ x.i + 2.0'))
+    add_two = primitive(
+        dex.eval(r'\x:((Fin 10)=>Float). for i. FToI $ x.i + 2.0'))
     x = jax.ShapeDtypeStruct((10,), np.float32)
     output_shape = jax.eval_shape(add_two, x)
     assert output_shape.shape == (10,)
@@ -37,7 +40,8 @@ class JAXTest(unittest.TestCase):
     np.testing.assert_allclose(jax.jit(add_two)(x), 2.0)
 
   def test_jit_array(self):
-    add_two = primitive(dex.eval(r'\x:((Fin 10)=>Float). for i. FToI $ x.i + 2.0'))
+    add_two = primitive(
+        dex.eval(r'\x:((Fin 10)=>Float). for i. FToI $ x.i + 2.0'))
     x = jnp.zeros((10,), dtype=np.float32)
     np.testing.assert_allclose(jax.jit(add_two)(x), (x + 2.0).astype(np.int32))
 
@@ -45,6 +49,37 @@ class JAXTest(unittest.TestCase):
     scale = primitive(dex.eval(r'\x:((Fin 10)=>Float) y:Float. for i. x.i * y'))
     x = jnp.arange(10, dtype=np.float32)
     np.testing.assert_allclose(scale(x, 5.0), x * 5.0)
+
+  def test_vmap(self):
+    add_two = primitive(dex.eval(r'\x:((Fin 2)=>Float). for i. x.i + 2.0'))
+    x = jnp.linspace([0, 3], [5, 8], num=4, dtype=jnp.float32)
+    np.testing.assert_allclose(jax.vmap(add_two)(x), x + 2.0)
+
+  def test_vmap_nonzero_index(self):
+    add_two = primitive(dex.eval(r'\x:((Fin 4)=>Float). for i. x.i + 2.0'))
+    x = jnp.linspace([0, 3], [5, 8], num=4, dtype=jnp.float32)
+    np.testing.assert_allclose(
+        jax.vmap(add_two, in_axes=1, out_axes=1)(x), x + 2.0)
+
+  def test_vmap_unbatched_array(self):
+    add_arrays = primitive(
+        dex.eval(r'\x:((Fin 10)=>Float) y:((Fin 10)=>Float). for i. x.i + y.i'))
+    x = jnp.arange(10, dtype=np.float32)
+    y = jnp.linspace(
+        jnp.arange(10), jnp.arange(10, 20), num=5, dtype=jnp.float32)
+    np.testing.assert_allclose(
+        jax.vmap(add_arrays, in_axes=[None, 0])(x, y), x + y)
+
+  def test_vmap_jit(self):
+    add_two = primitive(dex.eval(r'\x:((Fin 2)=>Float). for i. x.i + 2.0'))
+    x = jnp.linspace([0, 3], [5, 8], num=4, dtype=jnp.float32)
+    np.testing.assert_allclose(jax.jit(jax.vmap(add_two))(x), x + 2.0)
+
+  def test_vmap_jit_nonzero_index(self):
+    add_two = primitive(dex.eval(r'\x:((Fin 4)=>Float). for i. x.i + 2.0'))
+    x = jnp.linspace([0, 3], [5, 8], num=4, dtype=jnp.float32)
+    np.testing.assert_allclose(
+        jax.jit(jax.vmap(add_two, in_axes=1, out_axes=1))(x), x + 2.0)
 
 
 if __name__ == "__main__":
