638977687ba895fb424801d0b2d2debb7a4b372b
DHHZ <dhhzheng@google.com>
2021-06-25 16:00:09 +0100

    Add transpose to JAX interop.

--- python/tests/jax_test.py
+++ python/tests/jax_test.py
@@ -125,6 +125,86 @@ class JAXTest(unittest.TestCase):
     np.testing.assert_allclose(output_dex, output_jax)
     np.testing.assert_allclose(tangent_dex, tangent_jax)
 
+  def test_grad(self):
+    f_dex = primitive(dex.eval(
+        r'\x:((Fin 10) => Float). '
+        'sum $ for i. (IToF $ ordinal i) * x.i * x.i'))
+
+    def f_jax(x):
+      return jnp.sum(jnp.arange(10.) * x**2)
+
+    x = jnp.linspace(-0.2, 0.5, num=10)
+
+    grad_dex = jax.grad(f_dex)(x)
+    grad_jax = jax.grad(f_jax)(x)
+
+    np.testing.assert_allclose(grad_dex, grad_jax)
+
+  def test_grad_jit(self):
+    f_dex = primitive(dex.eval(
+        r'\x:((Fin 10) => Float). '
+        'sum $ for i. (IToF $ ordinal i) * x.i * x.i'))
+
+    def f_jax(x):
+      return jnp.sum(jnp.arange(10.) * x**2)
+
+    def grad_dex(x):
+      return jax.grad(f_dex)(x)
+
+    def grad_jax(x):
+      return jax.grad(f_jax)(x)
+
+    x = jnp.linspace(-0.2, 0.5, num=10)
+
+    grad_dex_jit = jax.jit(grad_dex)(x)
+    grad_jax_jit = jax.jit(grad_jax)(x)
+
+    np.testing.assert_allclose(grad_dex_jit, grad_jax_jit)
+
+  def test_grad_binary_function(self):
+    f_dex = primitive(dex.eval(
+        r'\x:((Fin 10) => Float) y:((Fin 10) => Float). '
+        'sum $ for i. x.i * x.i + 2.0 * y.i'))
+
+    def f_jax(x, y):
+      return jnp.sum(x**2 + 2 * y)
+
+    x  = jnp.arange(10.)
+    y = jnp.linspace(-0.2, 0.5, num=10)
+
+    np.testing.assert_allclose(
+        jax.grad(f_dex, argnums=1)(x, y),
+        jax.grad(f_jax, argnums=1)(x, y))
+
+    np.testing.assert_allclose(
+        jax.grad(f_dex, argnums=0)(x, y),
+        jax.grad(f_jax, argnums=0)(x, y))
+
+    np.testing.assert_allclose(
+        jax.grad(f_dex, argnums=(0, 1))(x, y),
+        jax.grad(f_jax, argnums=(0, 1))(x, y))
+
+  def test_grad_binary_function_jit(self):
+    f_dex = primitive(dex.eval(
+        r'\x:((Fin 10) => Float) y:((Fin 10) => Float). '
+        'sum $ for i. x.i * x.i + 2.0 * y.i'))
+
+    def f_jax(x, y):
+      return jnp.sum(x**2 + 2 * y)
+
+    def grad_dex(x, y):
+      return jax.grad(f_dex)(x, y)
+
+    def grad_jax(x, y):
+      return jax.grad(f_jax)(x, y)
+
+    x  = jnp.arange(10.)
+    y = jnp.linspace(-0.2, 0.5, num=10)
+
+    np.testing.assert_allclose(
+        jax.jit(grad_dex)(x, y),
+        jax.jit(grad_jax)(x, y))
+
 
 if __name__ == "__main__":
   unittest.main()
