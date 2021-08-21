c4cd3b46d0b6d88783fd42ff4659ce821ea7f5a0
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-08-21 10:56:07 +0100

    more tests

--- julia/test/runtests.jl
+++ julia/test/runtests.jl
@@ -1,16 +1,22 @@
 using Test
 using DexCall
 
-@testset "basic demo of eval and check errors" begin
-    context() do ctx
-        dex_eval(ctx, "1 + 1.0\n")
-        error_message = DexCall.get_error_msg()
-        @test contains(error_message, r"Type error.*Expected: Int32.*Actual: Float32.*1 \+ 1.0"s)
+@testset "DexCall" begin
+    @testset "basic demo of eval and check errors" begin
+        context() do ctx
+            dex_eval(ctx, "1 + 1.0\n")
+            error_message = DexCall.get_error_msg()
+            @test contains(error_message, r"Type error.*Expected: Int32.*Actual: Float32.*1 \+ 1.0"s)
+        end
     end
-end
 
-@testset "evaluate" begin
-    @test repr(evaluate("1")) == repr("1")
-    @test repr(evaluate("[1, 2]")) == repr("[1, 2]")
-    @test repr(evaluate("1+3")) == repr("4")
+    @testset "evaluate" begin
+        @test repr(evaluate("1")) == repr("1")
+        @test repr(evaluate("[1, 2]")) == repr("[1, 2]")
+        @test repr(evaluate("1+3")) == repr("4")
+        @test repr(evaluate("for i. [1, 2].i + 1")) == repr("[2, 3]")
+
+        @test_throws DexError evaluate("1 + 2.0")
+    end
+
 end
