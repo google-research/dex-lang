5be694eacbff430345100a49a9a09a24709eae17
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-08-22 22:22:43 +0100

    shrink DexCall API surface

--- julia/test/api.jl
+++ julia/test/api.jl
@@ -1,7 +1,7 @@
 @testset "api.jl" begin
     @testset "basic demo of eval and check errors" begin
-        context() do ctx
-            dex_eval(ctx, "1 + 1.0\n")
+        DexCall.context() do ctx
+            DexCall.dex_eval(ctx, "1 + 1.0\n")
             error_message = DexCall.get_error_msg()
             @test contains(error_message, r"Type error.*Expected: Int32.*Actual: Float32.*1 \+ 1.0"s)
         end
