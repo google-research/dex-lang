71e5bc5151606c3b319c20435d2108fdc0ada1a5
Lyndon White <oxinabox@ucc.asn.au>
2021-09-29 13:51:53 +0100

    use a custom primitive to hold tagged union payload

--- julia/src/api_types.jl
+++ julia/src/api_types.jl
@@ -22,8 +22,8 @@ end
 Represents a tagged union over types `A`, `B` etc.
 Must have a first field `tag::UInt64`
 and a second field `payload` which must be some isbits type that can hold the largest 
-element of the union (Probably a `NTuple{UInt8}` or similar).
-Julia doens't directly support Unions in the mappting to-from C
+element of the union, which you can (e.g.) declare as a custom `primative`.
+This is required as Julia doens't directly support Unions in the mapping to-from C
 so we store the data as arbitary bits then force it to be reinterpretted based on the tag
 """
 abstract type TaggedUnion{T<:Tuple} end
@@ -40,12 +40,13 @@ function force_reinterpret(::Type{T}, raw) where T
     return unsafe_load(Ptr{T}(Base.pointer_from_objref(Ref(raw))))
 end
 
-
+"Holds data for CLit, big enough to hold the larges of its union members, such as a Float64"
+primitive type CLitPayload 64 end
 struct CLit <: TaggedUnion{Tuple{Int64, Int32, UInt8, Float64, Float32, UInt32, UInt64}}
     tag::UInt64
-    payload::NTuple{8, UInt8}  # actually the Union, needs to be big enough to hold largest which is an Int64
+    payload::CLitPayload
 end
-CLit(x) = CLit(CLit_tag(x), force_reinterpret(NTuple{8, UInt8}, x))
+CLit(x) = CLit(CLit_tag(x), force_reinterpret(CLitPayload, x))
 
 CLit_tag(::Int64) = 0x0000_0000_0000_0000
 CLit_tag(::Int32) = 0x0000_0000_0000_0001
@@ -55,12 +56,13 @@ CLit_tag(::Float32) = 0x0000_0000_0000_0004
 CLit_tag(::UInt32) = 0x0000_0000_0000_0005
 CLit_tag(::UInt64) = 0x0000_0000_0000_0006
 
-
+"Holds data for CAtom, big enough to hold the larges of its union members, which is the CRectArray"
+primitive type CAtomPayload 3*64 end
 struct CAtom <: TaggedUnion{Tuple{CLit, CRectArray}}
     tag::UInt64
-    payload::NTuple{3, UInt64}  # actually the Union, needs to be big enough to hold largest which is the CRectArray
+    payload::CAtomPayload
 end
-CAtom(x) = CAtom(0x0000_0000_0000_0000, force_reinterpret(NTuple{3, UInt64}, CLit(x)))
+CAtom(x) = CAtom(0x0000_0000_0000_0000, force_reinterpret(CAtomPayload, CLit(x)))
 CAtom(x::AbstractArray) = throw(DomainError(typeof(x), "Arrays not yet supported"))
 function CAtom(atm::Ptr{HsAtom})
     result = Ref{CAtom}()
