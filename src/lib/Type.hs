b61a2c9ae628d3a6d22b970627135c0e9d41dc05
Alexey Radul <axch@google.com>
2022-03-24 06:47:23 -0400

    Delete a few unused explicit extensions.

--- src/lib/Type.hs
+++ src/lib/Type.hs
@@ -4,8 +4,6 @@
 -- license that can be found in the LICENSE file or at
 -- https://developers.google.com/open-source/licenses/bsd
 
-{-# LANGUAGE UndecidableInstances #-}
-
 module Type (
   HasType (..), CheckableE (..), CheckableB (..),
   checkTypes, checkTypesM,
