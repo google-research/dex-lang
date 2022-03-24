30745d1364eca045596b166e07455ea6cf13d303
Alexey Radul <axch@google.com>
2022-03-24 06:59:26 -0400

    Clean up extensions used in the Dex foreign library.

--- src/Dex/Foreign/JIT.hs
+++ src/Dex/Foreign/JIT.hs
@@ -4,8 +4,6 @@
 -- license that can be found in the LICENSE file or at
 -- https://developers.google.com/open-source/licenses/bsd
 
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Dex.Foreign.JIT (
