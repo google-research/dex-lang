38292b6e0d84cf1be7dd0e4670a805436013e0b4
joaogui1 <joaoguilhermearujo@gmail.com>
2022-09-11 14:22:00 -0300

    improved completion

--- misc/dex-completion.bash
+++ misc/dex-completion.bash
@@ -1,13 +1,26 @@
 #/usr/bin/env bash
 
+_get_dx_files()
+{
+    find . -name "*.dx"
+}
+
 _dex_completions()
 {
     COMPREPLY=();
     local word="${COMP_WORDS[COMP_CWORD]}";
     if [ "$COMP_CWORD" -eq 1 ]; then
         COMPREPLY=($(compgen -W "repl script web watch" -- "$word"));
+    elif [ "${COMP_WORDS[1]}" = "repl" ]; then
+        COMPREPLY=()
+    elif [ "${COMP_WORDS[1]}" = "script" ]; then
+        COMPREPLY=($(compgen -G "*.dx" -- "$word"));
+    elif [ "${COMP_WORDS[1]}" = "watch" ]; then
+        COMPREPLY=($(compgen -G "*.dx" -- "$word"));
+    elif [ "${COMP_WORDS[1]}" = "web" ]; then
+        COMPREPLY=($(compgen -G "*.dx" -- "$word"));
     else
         COMPREPLY=()
     fi
 }
-complete -o default -F _dex_completions dex
+complete -F _dex_completions dex
