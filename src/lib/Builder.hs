google-research/dex-lang

commit 6772b86657b0fe70100c497453b552586dc7b4a8

More edits suggested by Adam.

src/lib/Builder.hs
@@ -1335,16 +1335,16 @@ applyIxMethod dict method args = case dict of
     UnsafeFromOrdinal -> do
       [ix] <- return args                     -- ix : Nat
       return $ Con $ Newtype (TC $ Fin n) ix  -- result : Fin n
-  DictCon (ExplicitMethods _ fs params) -> do
+  DictCon (ExplicitMethods d params) -> do
+    SpecializedDictBinding _ fs <- lookupEnv d
     let f = fs !! fromEnum method
     let args' = case method of
           Size -> params ++ [UnitVal]
           _    -> params ++ args
-    Var v <- return f
-    TopFunBound _ (SpecializedTopFun (IxMethodSpecialization _ _)) <- lookupAtomName v
+    TopFunBound _ (SpecializedTopFun (IxMethodSpecialization _ _)) <- lookupAtomName f
     -- TODO(dougalm): make sure we can guarantee that this cache is already populated.
     -- Maybe we need to toposort based on dependencies somewhere?
-    Just fSimpName <- queryIxLoweredCache v
+    Just fSimpName <- queryIxLoweredCache f
     TopFunBound _ (LoweredTopFun (NaryLamExpr bs _ body)) <- lookupAtomName fSimpName
     emitBlock =<< applySubst (bs @@> fmap SubstVal args') body
   _ -> do

src/lib/CheckType.hs
@@ -172,6 +172,7 @@ instance Color c => CheckableE (Binding c) where
     EffectBinding     eff               -> EffectBinding     <$> substM eff
     HandlerBinding    h                 -> HandlerBinding    <$> substM h
     EffectOpBinding   op                -> EffectOpBinding   <$> substM op
+    SpecializedDictBinding ty methods   -> SpecializedDictBinding <$> substM ty <*> mapM substM methods
 
 instance CheckableE AtomBinding where
   checkE binding = case binding of
@@ -418,19 +419,19 @@ dictExprType e = case e of
   IxFin n -> do
     n' <- checkTypeE NatTy n
     liftM DictTy $ ixDictType $ TC $ Fin n'
-  ExplicitMethods ty methodFunNames args -> do
-    DictTy (DictType _ className params) <- checkTypeE TyKind (DictTy ty)
+  ExplicitMethods d args -> do
+    SpecializedDictBinding ty methodFunNames <- lookupEnv =<< substM d
+    DictType _ className params <- return ty
     ClassDef _ _ pbs (SuperclassBinders Empty _) methodTys <- lookupClassDef className
     forMZipped_ methodTys methodFunNames \(MethodType _ methodTy) methodFunName -> do
       reqTy <- checkedApplyNaryAbs (Abs pbs methodTy) params
       let args' = case reqTy of
             Pi _ -> args
             _ -> args ++ [UnitVal]
-      methodFunTy <- getType =<< substM methodFunName
+      methodFunTy <- getType $ Var methodFunName
       actualTy  <- checkApp methodFunTy args'
       checkAlphaEq reqTy actualTy
-      -- TODO: something special with thunks/nullary methods?
-    substM $ DictTy ty
+    return $ DictTy ty
 
 instance HasType DictExpr where
   getTypeE e = dictExprType e

src/lib/GenericTraversal.hs
@@ -162,7 +162,7 @@ instance GenericallyTraversableE DictExpr where
     InstantiatedGiven given args -> InstantiatedGiven <$> tge given <*> mapM tge args
     SuperclassProj subclass i -> SuperclassProj <$> tge subclass <*> pure i
     IxFin n ->  IxFin <$> tge n
-    ExplicitMethods ty methods params -> ExplicitMethods <$> tge ty <*> mapM tge methods <*> mapM tge params
+    ExplicitMethods d params -> ExplicitMethods <$> substM d <*> mapM tge params
 
 instance GenericallyTraversableE DictType where
   traverseGenericE (DictType sn cn params) = DictType sn <$> substM cn <*> mapM tge params

src/lib/Imp.hs
@@ -1546,7 +1546,8 @@ unsafeFromOrdinalImp (IxType _ dict) i = do
   i' <- (Con . Newtype NatTy) <$> toScalarAtom i
   case dict of
     DictCon (IxFin n) -> return $ Con $ Newtype (TC $ Fin n) i'
-    DictCon (ExplicitMethods _ fs params) ->
+    DictCon (ExplicitMethods d params) -> do
+      SpecializedDictBinding _ fs <- lookupEnv d
       appSpecializedIxMethod (fs !! fromEnum UnsafeFromOrdinal) (params ++ [i'])
     _ -> error $ "Not a simplified dict: " ++ pprint dict
 
@@ -1554,13 +1555,14 @@ indexSetSizeImp :: Emits n => IxType n -> SubstImpM i n (IExpr n)
 indexSetSizeImp (IxType _ dict) = do
   ans <- case dict of
     DictCon (IxFin n) -> return n
-    DictCon (ExplicitMethods _ fs params) ->
+    DictCon (ExplicitMethods d params) -> do
+      SpecializedDictBinding _ fs <- lookupEnv d
       appSpecializedIxMethod (fs !! fromEnum Size) (params ++ [UnitVal])
     _ -> error $ "Not a simplified dict: " ++ pprint dict
   fromScalarAtom $ unwrapBaseNewtype ans
 
-appSpecializedIxMethod :: Emits n => Atom n -> [Atom n] -> SubstImpM i n (Atom n)
-appSpecializedIxMethod ~(Var f) args = do
+appSpecializedIxMethod :: Emits n => AtomName n -> [Atom n] -> SubstImpM i n (Atom n)
+appSpecializedIxMethod f args = do
   Just fLoweredName <- queryIxLoweredCache f
   TopFunBound _ (LoweredTopFun (NaryLamExpr bs _ body)) <- lookupAtomName fLoweredName
   dropSubst $ extendSubst (bs @@> map SubstVal args) $ translateBlock Nothing body

src/lib/PPrint.hs
@@ -228,7 +228,7 @@ instance Pretty (DictExpr n) where
     InstantiatedGiven v args -> "Given" <+> p v <+> p (toList args)
     SuperclassProj d' i -> "SuperclassProj" <+> p d' <+> p i
     IxFin n -> "Ix (Fin" <+> p n <> ")"
-    ExplicitMethods _ methods args -> "ExplicitMethods" <+> p methods <+> p args
+    ExplicitMethods v args -> "ExplicitMethods" <+> p v <+> p args
 
 instance Pretty (DictType n) where
   pretty (DictType classSourceName _ params) =
@@ -489,6 +489,7 @@ instance Pretty (Binding s n) where
     EffectBinding _ -> "<effect-binding>"
     HandlerBinding _ -> "<handler-binding>"
     EffectOpBinding _ -> "<effect-op-binding>"
+    SpecializedDictBinding _ _ -> "<specialized-dict-binding>"
 
 instance Pretty (Module n) where
   pretty m = prettyRecord

src/lib/QueryType.hs
@@ -470,7 +470,9 @@ dictExprType e = case e of
   IxFin n -> do
     n' <- substM n
     liftM DictTy $ ixDictType $ TC $ Fin n'
-  ExplicitMethods ty _ _ -> substM $ DictTy ty
+  ExplicitMethods v _ -> do
+    SpecializedDictBinding ty _ <- lookupEnv =<< substM v
+    return $ DictTy ty
 
 getIxClassName :: (Fallible1 m, EnvReader m) => m n (ClassName n)
 getIxClassName = lookupSourceMap "Ix" >>= \case

src/lib/TopLevel.hs
@@ -962,13 +962,17 @@ instance GenericTraverser TopEnvFrag IxHoistingState where
     atom -> traverseAtomDefault atom
 
 emitIxDictSpecialization :: HoistingTopBuilder m => DictType n -> DictExpr n -> m n (DictExpr n)
-emitIxDictSpecialization _ d@(ExplicitMethods _ _ _) = return d -- Nothing to do if we already have explicit methods
-emitIxDictSpecialization _ d@(IxFin _)               = return d -- `Ix (Fin n))` is built-in
+emitIxDictSpecialization _ (ExplicitMethods _ _) = error "Should have specialized dicts yet"
+emitIxDictSpecialization _ d@(IxFin _)           = return d -- `Ix (Fin n))` is built-in
 emitIxDictSpecialization dictTy ixDict = do
   (ixDictAbs, params) <- generalizeIxDict ixDict
   methodImplNames <- forM [minBound..maxBound] \methodName ->
     getSpecializedFunction $ IxMethodSpecialization methodName ixDictAbs
-  return $ ExplicitMethods dictTy (map Var methodImplNames) params
+  maybeD <- liftTopBuilderHoisted $
+    emitBinding "d" $ sink $ SpecializedDictBinding dictTy methodImplNames
+  case maybeD of
+    Just d -> return $ ExplicitMethods d params
+    Nothing -> error "Couldn't hoist specialized dictionary"
 
 -- TODO: we could get more cache hits if we abstract more than necessary,
 -- based on whether parameters are data or nondata. E.g. abstract `Fin 10`

commit a086d57cc2fb5992d67be41ceae760965156b4b3

Fix a bug I accidentally introduced in 6772b86657.
The problem was that these `SpecializedDictBinding`s emissions can contain names
requiring specialization, and that wasn't captured by our free var querying. So
I made the change that tracks which specializations are needed explicitly. (We'd
been meaning to do this anyway.) As a result, I had to generalize
`HoistingTopBuilder` to allow emissions other than `TopEnvFrag`.

src/lib/Builder.hs
@@ -5,6 +5,7 @@
 -- https://developers.google.com/open-source/licenses/bsd
 
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Builder (
@@ -137,19 +138,19 @@ emitAtomToName hint x = emitHinted hint (Atom x)
 -- top-level function definitions, from within a local context. The operation
 -- may fail, returning `Nothing`, because the emission might mention local
 -- variables that can't be hoisted the top level.
-class (EnvReader m, MonadFail1 m) => HoistingTopBuilder m where
+class (EnvReader m, MonadFail1 m) => HoistingTopBuilder frag m | m -> frag where
   emitHoistedEnv :: (SinkableE e, SubstE Name e, HoistableE e)
-                 => Abs TopEnvFrag e n -> m n (Maybe (e n))
+                 => Abs frag e n -> m n (Maybe (e n))
   canHoistToTop :: HoistableE e => e n -> m n Bool
 
 liftTopBuilderHoisted
-  :: (HoistingTopBuilder m, SubstE Name e, SinkableE e, HoistableE e)
+  :: (EnvReader m, SubstE Name e, SinkableE e, HoistableE e)
   => (forall l. (Mut l, DExt n l) => TopBuilderM l (e l))
-  -> m n (Maybe (e n))
+  -> m n (Abs TopEnvFrag e n)
 liftTopBuilderHoisted cont = do
   env <- unsafeGetEnv
   Distinct <- getDistinct
-  emitHoistedEnv $ runHardFail $ runTopBuilderT env $ localTopBuilder cont
+  return $ runHardFail $ runTopBuilderT env $ localTopBuilder cont
 
 newtype DoubleBuilderT (topEmissions::B) (m::MonadKind) (n::S) (a:: *) =
   DoubleBuilderT { runDoubleBuilderT' :: DoubleInplaceT Env topEmissions BuilderEmissions m n a }
@@ -197,7 +198,8 @@ runDoubleBuilderT env cont = do
     runDoubleInplaceT env $ runDoubleBuilderT' cont
   return $ Abs envFrag e
 
-instance Fallible m => HoistingTopBuilder (DoubleBuilderT TopEnvFrag m) where
+instance (ExtOutMap Env f, OutFrag f, SubstB Name f, HoistableB f, Fallible m)
+  => HoistingTopBuilder f (DoubleBuilderT f m) where
   emitHoistedEnv ab = DoubleBuilderT $ emitDoubleInplaceTHoisted ab
   {-# INLINE emitHoistedEnv #-}
   canHoistToTop e = DoubleBuilderT $ canHoistToTopDoubleInplaceT e
@@ -245,7 +247,7 @@ instance ( SubstB Name frag, HoistableB frag, OutFrag frag
     return ans
   {-# INLINE refreshAbs #-}
 
-instance (SinkableV v, HoistingTopBuilder m) => HoistingTopBuilder (SubstReaderT v m i) where
+instance (SinkableV v, HoistingTopBuilder f m) => HoistingTopBuilder f (SubstReaderT v m i) where
   emitHoistedEnv ab = SubstReaderT $ lift $ emitHoistedEnv ab
   {-# INLINE emitHoistedEnv #-}
   canHoistToTop e = SubstReaderT $ lift $ canHoistToTop e
@@ -574,7 +576,8 @@ instance (SinkableE e, HoistableState e, ScopableBuilder m) => ScopableBuilder (
     return (Abs decls e, s'')
   {-# INLINE buildScoped #-}
 
-instance (SinkableE e, HoistableState e, HoistingTopBuilder m) => HoistingTopBuilder (StateT1 e m) where
+instance (SinkableE e, HoistableState e, HoistingTopBuilder frag m)
+  => HoistingTopBuilder frag (StateT1 e m) where
   emitHoistedEnv ab = lift11 $ emitHoistedEnv ab
   {-# INLINE emitHoistedEnv #-}
   canHoistToTop e = lift11 $ canHoistToTop e
@@ -1351,14 +1354,12 @@ applyIxMethod dict method args = case dict of
       [ix] <- return args                     -- ix : Nat
       return $ Con $ Newtype (TC $ Fin n) ix  -- result : Fin n
   DictCon (ExplicitMethods d params) -> do
-    SpecializedDictBinding _ fs <- lookupEnv d
+    SpecializedDictBinding (SpecializedDictDef _ fs) <- lookupEnv d
     let f = fs !! fromEnum method
     let args' = case method of
           Size -> params ++ [UnitVal]
           _    -> params ++ args
     TopFunBound _ (SpecializedTopFun (IxMethodSpecialization _ _)) <- lookupAtomName f
-    -- TODO(dougalm): make sure we can guarantee that this cache is already populated.
-    -- Maybe we need to toposort based on dependencies somewhere?
     Just fSimpName <- queryIxLoweredCache f
     TopFunBound _ (LoweredTopFun (NaryLamExpr bs _ body)) <- lookupAtomName fSimpName
     emitBlock =<< applySubst (bs @@> fmap SubstVal args') body

src/lib/CheckType.hs
@@ -172,7 +172,7 @@ instance Color c => CheckableE (Binding c) where
     EffectBinding     eff               -> EffectBinding     <$> substM eff
     HandlerBinding    h                 -> HandlerBinding    <$> substM h
     EffectOpBinding   op                -> EffectOpBinding   <$> substM op
-    SpecializedDictBinding ty methods   -> SpecializedDictBinding <$> substM ty <*> mapM substM methods
+    SpecializedDictBinding def          -> SpecializedDictBinding <$> substM def
 
 instance CheckableE AtomBinding where
   checkE binding = case binding of
@@ -423,18 +423,20 @@ dictExprType e = case e of
     n' <- checkTypeE NatTy n
     liftM DictTy $ ixDictType $ TC $ Fin n'
   ExplicitMethods d args -> do
-    SpecializedDictBinding ty methodFunNames <- lookupEnv =<< substM d
-    DictType _ className params <- return ty
+    SpecializedDictBinding def <- lookupEnv =<< substM d
+    SpecializedDictDef (Abs bs dictTy) methodFunNames <- return def
+    args' <- mapM substM args
+    dictTy'@(DictType _ className params) <- applySubst (bs @@> map SubstVal args') dictTy
     ClassDef _ _ pbs (SuperclassBinders Empty _) methodTys <- lookupClassDef className
     forMZipped_ methodTys methodFunNames \(MethodType _ methodTy) methodFunName -> do
       reqTy <- checkedApplyNaryAbs (Abs pbs methodTy) params
-      let args' = case reqTy of
+      let extendedArgs = case reqTy of
             Pi _ -> args
             _ -> args ++ [UnitVal]
       methodFunTy <- getType $ Var methodFunName
-      actualTy  <- checkApp methodFunTy args'
+      actualTy  <- checkApp methodFunTy extendedArgs
       checkAlphaEq reqTy actualTy
-    return $ DictTy ty
+    return $ DictTy dictTy'
 
 instance HasType DictExpr where
   getTypeE e = dictExprType e

src/lib/GenericTraversal.hs
@@ -29,9 +29,11 @@ liftGenericTraverserM s m =
 {-# INLINE liftGenericTraverserM #-}
 
 liftGenericTraverserMTopEmissions
-  :: (EnvReader m, SinkableE e, SubstE Name e, SinkableE s, SubstE Name s)  => s n
-  -> (forall l. DExt n l => GenericTraverserM TopEnvFrag s l l (e l))
-  -> m n (Abs TopEnvFrag (PairE e s) n)
+  :: ( EnvReader m, SinkableE e, SubstE Name e, SinkableE s
+     , SubstE Name s, ExtOutMap Env frag, OutFrag frag)
+  => s n
+  -> (forall l. DExt n l => GenericTraverserM frag s l l (e l))
+  -> m n (Abs frag (PairE e s) n)
 liftGenericTraverserMTopEmissions s m =
   liftM runHardFail $ liftDoubleBuilderT do
     (e, s') <- runStateT1 (runSubstReaderT idSubst $ runGenericTraverserM' m) (sink s)
@@ -48,7 +50,7 @@ deriving instance GenericTraverser f s => ScopeReader     (GenericTraverserM f s
 deriving instance GenericTraverser f s => EnvReader       (GenericTraverserM f s i)
 deriving instance GenericTraverser f s => ScopableBuilder (GenericTraverserM f s i)
 deriving instance GenericTraverser f s => Builder         (GenericTraverserM f s i)
-deriving instance GenericTraverser TopEnvFrag s => HoistingTopBuilder (GenericTraverserM TopEnvFrag s i)
+deriving instance GenericTraverser f s => HoistingTopBuilder f (GenericTraverserM f s i)
 
 class (SubstB Name f, HoistableB f, OutFrag f, ExtOutMap Env f, SinkableE s, HoistableState s)
       => GenericTraverser f s where

src/lib/Imp.hs
@@ -1550,7 +1550,7 @@ unsafeFromOrdinalImp (IxType _ dict) i = do
   case dict of
     DictCon (IxFin n) -> return $ Con $ Newtype (TC $ Fin n) i'
     DictCon (ExplicitMethods d params) -> do
-      SpecializedDictBinding _ fs <- lookupEnv d
+      SpecializedDictBinding (SpecializedDictDef _ fs) <- lookupEnv d
       appSpecializedIxMethod (fs !! fromEnum UnsafeFromOrdinal) (params ++ [i'])
     _ -> error $ "Not a simplified dict: " ++ pprint dict
 
@@ -1559,7 +1559,7 @@ indexSetSizeImp (IxType _ dict) = do
   ans <- case dict of
     DictCon (IxFin n) -> return n
     DictCon (ExplicitMethods d params) -> do
-      SpecializedDictBinding _ fs <- lookupEnv d
+      SpecializedDictBinding (SpecializedDictDef _ fs) <- lookupEnv d
       appSpecializedIxMethod (fs !! fromEnum Size) (params ++ [UnitVal])
     _ -> error $ "Not a simplified dict: " ++ pprint dict
   fromScalarAtom $ unwrapBaseNewtype ans

src/lib/PPrint.hs
@@ -499,7 +499,7 @@ instance Pretty (Binding s n) where
     EffectBinding _ -> "<effect-binding>"
     HandlerBinding _ -> "<handler-binding>"
     EffectOpBinding _ -> "<effect-op-binding>"
-    SpecializedDictBinding _ _ -> "<specialized-dict-binding>"
+    SpecializedDictBinding _ -> "<specialized-dict-binding>"
 
 instance Pretty (Module n) where
   pretty m = prettyRecord

src/lib/QueryType.hs
@@ -469,9 +469,11 @@ dictExprType e = case e of
   IxFin n -> do
     n' <- substM n
     liftM DictTy $ ixDictType $ TC $ Fin n'
-  ExplicitMethods v _ -> do
-    SpecializedDictBinding ty _ <- lookupEnv =<< substM v
-    return $ DictTy ty
+  ExplicitMethods v params -> do
+    params' <- mapM substM params
+    SpecializedDictBinding (SpecializedDictDef (Abs bs dictTy) _) <- lookupEnv =<< substM v
+    dictTy' <- applySubst (bs @@> map SubstVal params') dictTy
+    return $ DictTy dictTy'
 
 getIxClassName :: (Fallible1 m, EnvReader m) => m n (ClassName n)
 getIxClassName = lookupSourceMap "Ix" >>= \case

src/lib/Simplify.hs
@@ -7,8 +7,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module Simplify
-  ( simplifyTopBlock, simplifyTopFunction, SimplifiedBlock (..)
-  , Generalized, getSpecializedFunction) where
+  ( simplifyTopBlock, simplifyTopFunction, SimplifiedBlock (..), Generalized) where
 
 import Control.Category ((>>>))
 import Control.Monad
@@ -108,7 +107,7 @@ newtype SimplifyM (i::S) (o::S) (a:: *) = SimplifyM
     :: SubstReaderT AtomSubstVal (DoubleBuilderT TopEnvFrag (ReaderT WillLinearize HardFailM)) i o a }
   deriving ( Functor, Applicative, Monad, ScopeReader, EnvExtender, Fallible
            , EnvReader, SubstReader AtomSubstVal, MonadFail
-           , Builder, HoistingTopBuilder)
+           , Builder, HoistingTopBuilder TopEnvFrag)
 
 liftSimplifyM
   :: (SinkableE e, SubstE Name e, TopBuilder m, Mut n)
@@ -328,7 +327,8 @@ simplifyApp hint f xs =
             case splitAtExact numSpecializationArgs (toList xs) of
               Just (specializationArgs, runtimeArgs) -> do
                 (spec, extraArgs) <- determineSpecializationSpec v specializationArgs
-                specializedFunction <- getSpecializedFunction spec
+                ab <- getSpecializedFunction spec
+                Just specializedFunction <- emitHoistedEnv ab
                 naryAppHinted hint (Var specializedFunction)
                   (extraArgs ++ runtimeArgs)
               Nothing -> error $ "Specialization of " ++ pprint atom ++
@@ -442,17 +442,11 @@ generalizeInstance (DictType "Ix" _ [TC (Fin n)]) (DictCon (IxFin _)) =
   return $ DictCon (IxFin n)
 generalizeInstance _ x = return x
 
-getSpecializedFunction :: HoistingTopBuilder m => SpecializationSpec n -> m n (AtomName n)
+getSpecializedFunction :: EnvReader m => SpecializationSpec n -> m n (Abs TopEnvFrag AtomName n)
 getSpecializedFunction s = do
   querySpecializationCache s >>= \case
-    Just name -> return name
-    _ -> do
-      maybeName <- liftTopBuilderHoisted $ emitSpecialization (sink s)
-      case maybeName of
-        Nothing -> error $ "couldn't hoist specialization: " ++ pprint s ++
-          "\nFree vars: " ++ pprint (freeAtomVarsList s)
-          ++ "\n\n" ++ show s
-        Just name -> return name
+    Just name -> return $ Abs emptyOutFrag name
+    _ -> liftTopBuilderHoisted $ emitSpecialization (sink s)
 
 -- TODO: de-dup this and simplifyApp?
 simplifyTabApp :: forall i o. Emits o

src/lib/TopLevel.hs
@@ -617,13 +617,6 @@ evalRequiredSpecializations e = do
         queryImpCache v >>= \case
           Nothing -> compileTopLevelFun v
           Just _ -> return ()
-      -- XXX: we might need to toposort these based on dependencies. Or else
-      -- maybe we can be careful about the order in which we request them in the
-      -- first place (rather than querying free vars).
-      TopFunBound _ (SpecializedTopFun (IxMethodSpecialization _ _)) -> do
-        queryIxLoweredCache v >>= \case
-          Nothing -> lowerIxMethod v
-          Just _ -> return ()
       _ -> return ()
 
 execUDecl
@@ -985,16 +978,28 @@ hoistIxDicts
      , SinkableE e, SubstE Name e, HoistableE e)
   => e n -> m n (e n)
 hoistIxDicts e = do
-  Abs frag (PairE e' (IxHoistingState UnitE)) <-
+  Abs (IxHoistingEmissions envFrag methodNames) (PairE e' (IxHoistingState UnitE)) <-
     liftGenericTraverserMTopEmissions (IxHoistingState UnitE) $ traverseGenericE (sink e)
-  e'' <- emitEnv (Abs frag e')
-  evalRequiredSpecializations e''  -- TODO: track the needed sepcializations rather than traversing again to find them
+  (PairE (ListE methodNames') e'') <- emitEnv $ Abs envFrag (PairE (ListE methodNames) e')
+  forM_ methodNames' \v -> do
+    lookupAtomName v >>= \case
+      TopFunBound _ (SpecializedTopFun (IxMethodSpecialization _ _)) -> do
+        queryIxLoweredCache v >>= \case
+          Nothing -> lowerIxMethod v
+          Just _ -> return ()
+      _ -> error "Expected a method name"
   return e''
+
+type IxHoistingM = GenericTraverserM IxHoistingEmissions IxHoistingState
+
+data IxHoistingEmissions (n::S) (l::S) =
+  -- env frag, names of specialized methods we need to lower
+  IxHoistingEmissions (TopEnvFrag n l) [AtomName l]
 
 newtype IxHoistingState (n::S) = IxHoistingState (UnitE n)
         deriving (SinkableE, HoistableState, SubstE Name)
 
-instance GenericTraverser TopEnvFrag IxHoistingState where
+instance GenericTraverser IxHoistingEmissions IxHoistingState where
   traverseAtom = \case
     DictCon d -> do
       d' <- substM d
@@ -1004,15 +1009,21 @@ instance GenericTraverser TopEnvFrag IxHoistingState where
         _ -> return d'
     atom -> traverseAtomDefault atom
 
-emitIxDictSpecialization :: HoistingTopBuilder m => DictType n -> DictExpr n -> m n (DictExpr n)
-emitIxDictSpecialization _ (ExplicitMethods _ _) = error "Should have specialized dicts yet"
+emitIxDictSpecialization :: DictType n -> DictExpr n -> IxHoistingM i n (DictExpr n)
+emitIxDictSpecialization _ d@(ExplicitMethods _ _) = return d
 emitIxDictSpecialization _ d@(IxFin _)           = return d -- `Ix (Fin n))` is built-in
 emitIxDictSpecialization dictTy ixDict = do
-  (ixDictAbs, params) <- generalizeIxDict ixDict
-  methodImplNames <- forM [minBound..maxBound] \methodName ->
-    getSpecializedFunction $ IxMethodSpecialization methodName ixDictAbs
-  maybeD <- liftTopBuilderHoisted $
-    emitBinding "d" $ sink $ SpecializedDictBinding dictTy methodImplNames
+  (Abs bs (PairE dictTy' ixDict'), params) <- generalizeIxDict dictTy ixDict
+  (Abs frag (PairE (ListE methodNames) dictName)) <- liftTopBuilderHoisted do
+    methodImplNames <- forM [minBound..maxBound] \methodName -> do
+      let s = IxMethodSpecialization methodName (sink (Abs bs (DictCon ixDict')))
+      querySpecializationCache s >>= \case
+        Just name -> return name
+        _ -> emitSpecialization s
+    d <- emitBinding "d" $ sink $ SpecializedDictBinding $
+           SpecializedDictDef (sink (Abs bs dictTy')) methodImplNames
+    return $ PairE (ListE methodImplNames) d
+  maybeD <- emitHoistedEnv $ Abs (IxHoistingEmissions frag methodNames) dictName
   case maybeD of
     Just d -> return $ ExplicitMethods d params
     Nothing -> error "Couldn't hoist specialized dictionary"
@@ -1020,9 +1031,10 @@ emitIxDictSpecialization dictTy ixDict = do
 -- TODO: we could get more cache hits if we abstract more than necessary,
 -- based on whether parameters are data or nondata. E.g. abstract `Fin 10`
 -- as `(\n. Fin n) 10` instead of not abstracting it at all.
-generalizeIxDict :: (HoistingTopBuilder m, EnvReader m) => DictExpr n -> m n (Generalized Atom n)
-generalizeIxDict d = do
-  typedVs <- forMFilter (freeAtomVarsList d) \v ->
+generalizeIxDict :: DictType n -> DictExpr n -> IxHoistingM i n (Generalized (PairE DictType DictExpr) n)
+generalizeIxDict dTy d = do
+  let dWithTy = PairE dTy d
+  typedVs <- forMFilter (freeAtomVarsList dWithTy) \v ->
     canHoistToTop v >>= \case
       False -> do
         ty <- getType v
@@ -1030,7 +1042,29 @@ generalizeIxDict d = do
       True -> return Nothing
   let typedVsToposorted = toposortAnnVars typedVs
   let atoms = [Var v | (v, _) <- typedVsToposorted]
-  return (abstractFreeVars typedVsToposorted (DictCon d), atoms)
+  return (abstractFreeVars typedVsToposorted dWithTy, atoms)
+
+instance GenericB IxHoistingEmissions where
+  type RepB IxHoistingEmissions = PairB TopEnvFrag (LiftB (ListE AtomName))
+  fromB (IxHoistingEmissions frag names) = PairB frag (LiftB (ListE names))
+  toB   (PairB frag (LiftB (ListE names))) = IxHoistingEmissions frag names
+
+instance ProvesExt   IxHoistingEmissions
+instance SinkableB   IxHoistingEmissions
+instance HoistableB  IxHoistingEmissions
+instance BindsNames  IxHoistingEmissions
+instance SubstB Name IxHoistingEmissions
+
+instance OutFrag IxHoistingEmissions where
+  emptyOutFrag = IxHoistingEmissions emptyOutFrag []
+  {-# INLINE emptyOutFrag #-}
+  catOutFrags s (IxHoistingEmissions frag1 names1) (IxHoistingEmissions frag2 names2) =
+    withExtEvidence frag2 $
+      IxHoistingEmissions (catOutFrags s frag1 frag2) (map sink names1 <> names2)
+  {-# INLINE catOutFrags #-}
+
+instance ExtOutMap Env IxHoistingEmissions where
+  extendOutMap env (IxHoistingEmissions frag1 _) = extendOutMap env frag1
 
 -- === instances ===
 

src/lib/Types/Core.hs
@@ -469,7 +469,7 @@ data Binding (c::C) (n::S) where
   FunObjCodeBinding :: FunObjCode -> LinktimeNames n  -> Binding FunObjCodeNameC n
   ModuleBinding     :: Module n                       -> Binding ModuleNameC     n
   PtrBinding        :: PtrLitVal                      -> Binding PtrNameC        n
-  SpecializedDictBinding :: DictType n -> [AtomName n] -> Binding SpecializedDictNameC n
+  SpecializedDictBinding :: SpecializedDictDef n      -> Binding SpecializedDictNameC n
 deriving instance Show (Binding c n)
 
 data EffectOpDef (n::S) where
@@ -557,6 +557,24 @@ instance SubstE Name EffectOpType
 instance SubstE AtomSubstVal EffectOpType
 deriving instance Show (EffectOpType n)
 deriving via WrapE EffectOpType n instance Generic (EffectOpType n)
+
+data SpecializedDictDef n =
+  -- dict type abstracted over local params, method names
+  SpecializedDictDef (Abs (Nest Binder) DictType n) [AtomName n]
+  deriving (Show, Generic)
+
+instance GenericE SpecializedDictDef where
+  type RepE SpecializedDictDef = PairE (Abs (Nest Binder) DictType) (ListE AtomName)
+  fromE (SpecializedDictDef ab methods) = ab `PairE` ListE methods
+  {-# INLINE fromE #-}
+  toE   (ab `PairE` ListE methods) = SpecializedDictDef ab methods
+  {-# INLINE toE #-}
+
+instance SinkableE      SpecializedDictDef
+instance HoistableE     SpecializedDictDef
+instance AlphaEqE       SpecializedDictDef
+instance AlphaHashableE SpecializedDictDef
+instance SubstE Name    SpecializedDictDef
 
 data AtomBinding (n::S) =
    LetBound    (DeclBinding   n)
@@ -1893,7 +1911,7 @@ instance Color c => GenericE (Binding c) where
           (EffectDef)
           (HandlerDef)
           (EffectOpDef)
-          (DictType `PairE` ListE AtomName)
+          (SpecializedDictDef)
       )
   fromE binding = case binding of
     AtomNameBinding   tyinfo            -> Case0 $ Case0 $ tyinfo
@@ -1910,7 +1928,7 @@ instance Color c => GenericE (Binding c) where
     EffectBinding   effDef              -> Case1 $ Case4 $ effDef
     HandlerBinding  hDef                -> Case1 $ Case5 $ hDef
     EffectOpBinding opDef               -> Case1 $ Case6 $ opDef
-    SpecializedDictBinding ty methods   -> Case1 $ Case7 $ ty `PairE` ListE methods
+    SpecializedDictBinding def          -> Case1 $ Case7 $ def
   {-# INLINE fromE #-}
 
   toE rep = case rep of
@@ -1928,7 +1946,7 @@ instance Color c => GenericE (Binding c) where
     Case1 (Case4 effDef)                                    -> fromJust $ tryAsColor $ EffectBinding     effDef
     Case1 (Case5 hDef)                                      -> fromJust $ tryAsColor $ HandlerBinding    hDef
     Case1 (Case6 opDef)                                     -> fromJust $ tryAsColor $ EffectOpBinding   opDef
-    Case1 (Case7 (ty `PairE` ListE methods))                -> fromJust $ tryAsColor $ SpecializedDictBinding ty methods
+    Case1 (Case7 def)                                       -> fromJust $ tryAsColor $ SpecializedDictBinding def
     _ -> error "impossible"
   {-# INLINE toE #-}
 
@@ -2245,6 +2263,7 @@ instance Store (BoxPtr n)
 instance (Store (ann n)) => Store (NonDepNest ann n l)
 instance Store Projection
 instance Store IxMethod
+instance Store (SpecializedDictDef n)
 
 -- === Orphan instances ===
 -- TODO: Resolve this!