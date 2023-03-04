orgTestCodacy11KRepos110MB/repo-2910-dex-lang

[WIP] Initial support for tooltips.
Dex has a reactive notebook and literate programming environment in `dex web`.

Tooltips add additional IDE information to the web environment, visible when
hovering the cursor over code.

This initial PR adds support for viewing expression scopes.
- Backend: compiler produces a `SpanTree` describing all nested expression
  scope ranges.
- Frontend: HTML rendering uses `SpanTree` to augment HTML elements with class
  attributes about scope ranges. Scope range class attributes have CSS styles
  for visualization purposes.

More details coming soon in a GitHub issue.

Co-Authored-By: Dougal <d.maclaurin@gmail.com>

src/lib/RenderHtml.hs
@@ -5,18 +5,21 @@
 -- https://developers.google.com/open-source/licenses/bsd
 
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
-module RenderHtml (pprintHtml, progHtml, ToMarkup) where
+module RenderHtml (pprintHtml, progHtml, ToMarkup, treeToHtml') where
 
-import Text.Blaze.Html5 as H  hiding (map)
+import Text.Blaze.Html5 as H hiding (map)
 import Text.Blaze.Html5.Attributes as At
 import Text.Blaze.Html.Renderer.String
+import Data.List    qualified as L
 import Data.Text    qualified as T
 import Data.Text.IO qualified as T
 import CMark (commonmarkToHtml)
 import System.IO.Unsafe
 
 import Control.Monad
+import Debug.Trace
 import Text.Megaparsec hiding (chunk)
 import Text.Megaparsec.Char as C
 
@@ -25,7 +28,10 @@ import Syntax
 import PPrint
 import Parser
 import Serialize ()
+import TraverseContexts
 import Err
+import Util
+import GHC.Exts (fromString)
 
 cssSource :: T.Text
 cssSource = unsafePerformIO $
@@ -61,7 +67,7 @@ wrapBody blocks = docTypeHtml $ do
     jsSource = textValue $ javascriptSource <> "render(RENDER_MODE.STATIC);"
 
 instance ToMarkup Result where
-  toMarkup (Result outs err) = foldMap toMarkup outs <> err'
+  toMarkup (Result outs err _) = foldMap toMarkup outs <> err'
     where err' = case err of
                    Failure e  -> cdiv "err-block" $ toHtml $ pprint e
                    Success () -> mempty
@@ -74,6 +80,12 @@ instance ToMarkup Output where
 instance ToMarkup SourceBlock where
   toMarkup block = case sbContents block of
     ProseBlock s -> cdiv "prose-block" $ mdToHtml s
+    -- NOTE: Add more cases here.
+    -- Options:
+    -- 1. Faithfully print UDecl, requires storing whitespace and trivia.
+    -- 2. Pass UDecl and string. We will do this.
+    EvalUDecl udecl -> renderUDecl udecl block
+    Command _ uexpr -> renderUExpr uexpr block
     _ -> cdiv "code-block" $ highlightSyntax (sbText block)
 
 mdToHtml :: T.Text -> Html
@@ -83,6 +95,120 @@ cdiv :: String -> Html -> Html
 cdiv c inner = H.div inner ! class_ (stringValue c)
 
 -- === syntax highlighting ===
+
+spanDelimitedCode :: SourceBlock -> [SrcPosCtx] -> Html
+spanDelimitedCode block ctxs =
+  let (Just tree) = srcCtxsToTree block ctxs in
+  spanDelimitedCode' block tree
+
+spanDelimitedCode' :: SourceBlock -> SpanTree () -> Html
+spanDelimitedCode' block tree =
+  let source' = sbText block in
+  treeToHtml' source' tree
+
+{-
+treeToHtml :: SourceBlock -> SpanTree () -> Html
+treeToHtml block = treeToHtml' (sbText block)
+-}
+
+treeToHtml' :: T.Text -> SpanTree () -> Html
+treeToHtml' source' tree =
+  -- case tree of
+  let tree' = fillTreeAndAddTrivialLeaves (T.unpack source') tree in
+  trace ("Tree: " ++ show tree')
+  treeToHtml'' source' tree'
+
+treeToHtml'' :: T.Text -> SpanTree a -> Html
+treeToHtml'' source' tree = case tree of
+  Span (_, _, spanId) children ->
+    let body' = foldMap (treeToHtml'' source') children in
+    createSpan (Just spanId) body' ! spanClass
+  Leaf (l, r, spanId) _ ->
+    let spanText = sliceText l r source' in
+    -- Note: only leaves need the code-span class.
+    createSpan (Just spanId) (highlightSyntax spanText) ! spanLeaf
+    -- createSpan (Just spanId) (toHtml spanText)
+  Trivia (l, r) _ ->
+    let spanText = sliceText l r source' in
+    createSpan Nothing (highlightSyntax spanText)
+    -- createSpan Nothing (toHtml spanText)
+  where createSpan :: Maybe SpanId -> Html -> Html
+        createSpan spanId body' = case spanId of
+          Nothing -> H.span body'
+          Just id' -> H.span body' ! spanIdAttribute id'
+        spanIdAttribute :: SpanId -> Attribute
+        spanIdAttribute spanId =
+          At.id (fromString (show spanId))
+        spanLeaf :: Attribute
+        spanLeaf = At.class_ "code-span-leaf"
+        spanClass :: Attribute
+        spanClass = At.class_ "code-span"
+
+srcCtxsToSpanInfos :: SourceBlock -> [SrcPosCtx] -> [SpanPayload]
+srcCtxsToSpanInfos block ctxs =
+  let blockOffset = sbOffset block in
+  let ctxs' = L.sort ctxs in
+  -- map (convert blockOffset) ctxs
+  (0, maxBound, -1) : mapMaybe (convert' blockOffset) ctxs'
+  where convert' :: Int -> SrcPosCtx -> Maybe SpanPayload
+        convert' offset (SrcPosCtx (Just (l, r)) (Just spanId) _) = Just (l - offset, r - offset, spanId)
+        convert' _ _ = Nothing
+
+srcCtxsToTree :: SourceBlock -> [SrcPosCtx] -> Maybe (SpanTree ())
+srcCtxsToTree block ctxs =
+  let infos = srcCtxsToSpanInfos block ctxs in
+  -- TODO: Use `makeSpanTree`? Need to update result type
+  makeEmptySpanTree $ trace ("[SpanInfo]:\n" ++ stringifyList infos) infos
+  -- makeSpanTree $ trace ("[SpanInfo]:\n" ++ stringifyList infos) infos
+
+stringifyList :: Show a => [a] -> String
+stringifyList xs = L.intercalate "\n" (map show xs)
+
+renderUDecl :: UDecl n n' -> SourceBlock -> Html
+renderUDecl udecl block =
+  let source' = sbText block in
+  let udecl' = addContextIds udecl in
+  let ctxs = gatherContexts udecl' in
+  -- TODO(danielzheng): Ensure that no two SrcPosCtx have overlapping spans.
+  -- TODO(danielzheng): How to map spans to html source range?
+  -- toHtml $ cdiv "code-block" $ highlightSyntax source
+  -- toHtml $ cdiv "code-block" $ trace ("[SrcPosCtx]: " ++ show ctxs) highlightSyntax source
+  -- toHtml $ cdiv "code-block" $ trace ("[SrcPosCtx]:\n" ++ show (sbOffset block) ++ "\n" ++ source' ++ "\n" ++ stringifyList ctxs ++ "\n" ++ stringifyList (contextsToSpanEndpoints ctxs)) highlightSyntax source'
+  toHtml $ cdiv "code-block" $ trace (
+    "[SrcPosCtx]:\n" ++ show (sbOffset block) ++ "\n" ++ T.unpack source' ++ "\n" ++
+    stringifyList ctxs
+    ++ "\n" -- ++ renderHtml (spanDelimitedCode block ctxs)
+  )
+  -- highlightSyntax source'
+  spanDelimitedCode block ctxs
+
+renderUExpr :: UExpr n -> SourceBlock -> Html
+renderUExpr uexpr block =
+  let source' = sbText block in
+  let uexpr' = addContextIds uexpr in
+  let ctxs = gatherContexts uexpr' in
+  -- toHtml $ cdiv "code-block" $ highlightSyntax source
+  -- toHtml $ cdiv "code-block" $ trace ("[SrcPosCtx]: " ++ show ctxs) highlightSyntax source
+  -- toHtml $ cdiv "code-block" $ trace ("[SrcPosCtx]:\n" ++ L.intercalate "\n" (map show ctxs)) highlightSyntax source
+  toHtml $ cdiv "code-block" $ trace (
+    "[SrcPosCtx]:\n" ++ show (sbOffset block) ++ "\n" ++ T.unpack source' ++ "\n" ++
+    stringifyList ctxs
+    ++ "\n" -- ++ renderHtml (spanDelimitedCode block ctxs)
+  )
+  -- highlightSyntax source'
+  spanDelimitedCode block ctxs
+  -- 1. We have `source`
+  -- 2. We have source ranges from UDecl
+  --    - Expressions within UDecl (e.g. instance methods) should have annotated
+  --      SrcPos at this point.
+  --    - May want a function that traverses UDecl to collect all annotations
+  -- 3. Produce HTML
+
+-- -- TODO: This needs to be beefed up to accept annotation data.
+-- -- NOTE: This is called per block.
+-- highlightSyntax' :: (String, UExpr) -> Html
+-- highlightSyntax' s = foldMap (uncurry syntaxSpan) classified
+--   where classified = ignoreExcept $ parseit s (many (withSource classify) <* eof)
 
 highlightSyntax :: T.Text -> Html
 highlightSyntax s = foldMap (uncurry syntaxSpan) classified
@@ -99,11 +225,12 @@ syntaxSpan s c = H.span (toHtml s) ! class_ (stringValue className)
       SymbolStr   -> "symbol"
       TypeNameStr -> "type-name"
       IsoSugarStr -> "iso-sugar"
+      WhitespaceStr -> "whitespace"
       NormalStr -> error "Should have been matched already"
 
 data StrClass = NormalStr
               | CommentStr | KeywordStr | CommandStr | SymbolStr | TypeNameStr
-              | IsoSugarStr
+              | IsoSugarStr | WhitespaceStr
 
 classify :: Parser StrClass
 classify =
@@ -115,6 +242,7 @@ classify =
    <|> try (char '#' >> (char '?' <|> char '&' <|> char '|' <|> pure ' ')
         >> lowerWord >> return IsoSugarStr)
    <|> (some symChar >> return SymbolStr)
+   <|> (some space1 >> return WhitespaceStr)
    <|> (anySingle >> return NormalStr)
 
 lowerWord :: Parser String

google-research/dex-lang

src/lib/RenderHtml.hs
@@ -5,12 +5,14 @@
 -- https://developers.google.com/open-source/licenses/bsd
 
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
-module RenderHtml (pprintHtml, progHtml, ToMarkup) where
+module RenderHtml (pprintHtml, progHtml, ToMarkup, treeToHtml', drawTree) where
 
-import Text.Blaze.Html5 as H  hiding (map)
+import Text.Blaze.Html5 as H hiding (map)
 import Text.Blaze.Html5.Attributes as At
 import Text.Blaze.Html.Renderer.String
+import Data.List    qualified as L
 import Data.Text    qualified as T
 import Data.Text.IO qualified as T
 import CMark (commonmarkToHtml)
@@ -24,8 +26,12 @@ import Err
 import Lexing (Parser, symChar, keyWordStrs, symbol, parseit, withSource)
 import Paths_dex  (getDataFileName)
 import PPrint ()
+import SourceInfo
+import TraverseContexts
 import Types.Misc
 import Types.Source
+import Util
+import Debug.Trace
 
 cssSource :: T.Text
 cssSource = unsafePerformIO $
@@ -72,9 +78,11 @@ instance ToMarkup Output where
     _ -> cdiv "result-block" $ toHtml $ pprint out
 
 instance ToMarkup SourceBlock where
-  toMarkup block = case sbContents block of
+  toMarkup sourceBlock = case sbContents sourceBlock of
     (Misc (ProseBlock s)) -> cdiv "prose-block" $ mdToHtml s
-    _ -> cdiv "code-block" $ highlightSyntax (sbText block)
+    TopDecl decl -> renderWithContexts decl sourceBlock
+    Command _ expr -> renderWithContexts expr sourceBlock
+    _ -> cdiv "code-block" $ highlightSyntax (sbText sourceBlock)
 
 mdToHtml :: T.Text -> Html
 mdToHtml s = preEscapedText $ commonmarkToHtml [] s
@@ -83,6 +91,125 @@ cdiv :: String -> Html -> Html
 cdiv c inner = H.div inner ! class_ (stringValue c)
 
 -- === syntax highlighting ===
+
+-- Note for pair programming: this is like the main function
+spanDelimitedCode :: SourceBlock -> [SrcPosCtx] -> Html
+spanDelimitedCode block ctxs =
+  let (Just tree) = srcCtxsToTree block ctxs in
+  spanDelimitedCode' block tree
+
+spanDelimitedCode' :: SourceBlock -> SpanTree -> Html
+spanDelimitedCode' block tree =
+  let source' = sbText block in
+  treeToHtml' source' tree
+
+{-
+treeToHtml :: SourceBlock -> SpanTree -> Html
+treeToHtml block = treeToHtml' (sbText block)
+-}
+
+drawTree :: SpanTree -> String
+drawTree tree = drawTree' 0 tree
+
+drawTree' :: Int -> SpanTree -> String
+drawTree' indent tree =
+  let prefix = replicate indent ' ' in
+  case tree of
+    Span info children ->
+      prefix ++ "Span " ++ show info ++ " [\n" ++
+      L.intercalate "\n" (map (drawTree' (indent + 2)) children)
+      ++ "\n" ++ prefix ++ "]"
+    LeafSpan _ -> prefix ++ show tree
+    Trivia _ -> prefix ++ show tree
+
+treeToHtml' :: T.Text -> SpanTree -> Html
+treeToHtml' source' tree =
+  let tree' = fillTreeAndAddTrivialLeaves (T.unpack source') tree in
+  -- trace ("Tree before:\n" ++ show tree ++ "\nTree after:\n" ++ show tree')
+  trace ("Tree before:\n" ++ drawTree tree ++ "\nTree after:\n" ++ drawTree tree')
+  treeToHtml'' source' tree'
+
+treeToHtml'' :: T.Text -> SpanTree -> Html
+treeToHtml'' source' tree = case tree of
+  Span (_, _, spanId) children ->
+    let body' = foldMap (treeToHtml'' source') children in
+    createSpan (Just spanId) body' ! spanClass
+  LeafSpan (l, r, spanId) ->
+    let spanText = sliceText l r source' in
+    -- Note: only leaves need the code-span class.
+    createSpan (Just spanId) (highlightSyntax spanText) ! spanLeaf
+  Trivia (l, r) ->
+    let spanText = sliceText l r source' in
+    createSpan Nothing (highlightSyntax spanText)
+  where createSpan :: Maybe SpanId -> Html -> Html
+        createSpan spanId body' = case spanId of
+          Nothing -> H.span body'
+          Just id' -> H.span body' ! spanIdAttribute id'
+        spanIdAttribute :: SpanId -> Attribute
+        spanIdAttribute spanId =
+          At.id (stringValue (show spanId))
+        spanLeaf :: Attribute
+        spanLeaf = At.class_ "code-span-leaf"
+        spanClass :: Attribute
+        spanClass = At.class_ "code-span"
+
+srcCtxsToSpanInfos :: SourceBlock -> [SrcPosCtx] -> [SpanPayload]
+srcCtxsToSpanInfos block ctxs =
+  let blockOffset = sbOffset block in
+  let ctxs' = L.sort ctxs in
+  -- map (convert blockOffset) ctxs
+  (0, maxBound, 0) : mapMaybe (convert' blockOffset) ctxs'
+  where convert' :: Int -> SrcPosCtx -> Maybe SpanPayload
+        convert' offset (SrcPosCtx (Just (l, r)) (Just spanId)) = Just (l - offset, r - offset, spanId + 1)
+        convert' _ _ = Nothing
+
+
+srcCtxsToTree :: SourceBlock -> [SrcPosCtx] -> Maybe SpanTree
+srcCtxsToTree block ctxs =
+  let infos = srcCtxsToSpanInfos block ctxs in
+  -- NOTE(danielzheng): Critical line here
+  let infos' = infos in
+  -- let infos' = fixSpanPayloads infos in
+  trace ("HELLO srcCtxsToTree infos:\n" ++ show infos ++ "\ninfos':\n" ++ show infos')
+  makeEmptySpanTree infos'
+
+renderWithContexts :: HasContexts a => a -> SourceBlock -> Html
+renderWithContexts decl block =
+  let decl' = addContextIds decl in
+  let ctxs = gatherContexts decl' in
+  toHtml $ cdiv "code-block" $ spanDelimitedCode block ctxs
+  -- toHtml $ cdiv "code-block" $ trace (
+  --   "[SrcPosCtx]:\n" ++ show (sbOffset block) ++ "\n" ++ T.unpack (sbText block) ++ "\n" ++
+  --   stringifyList ctxs
+  --   ++ "\n" -- ++ renderHtml (spanDelimitedCode block ctxs)
+  -- )
+  -- spanDelimitedCode block ctxs
+
+renderDecl :: CTopDecl -> SourceBlock -> Html
+renderDecl decl block =
+  let decl' = addContextIds decl in
+  let ctxs = gatherContexts decl' in
+  -- toHtml $ cdiv "code-block" $ spanDelimitedCode block ctxs
+  toHtml $ cdiv "code-block" $ trace (
+    "[SrcPosCtx]:\n" ++ show (sbOffset block) ++ "\n" ++ T.unpack (sbText block) ++ "\n" ++
+    stringifyList ctxs
+    ++ "\n" -- ++ renderHtml (spanDelimitedCode block ctxs)
+  )
+  spanDelimitedCode block ctxs
+
+stringifyList :: Show a => [a] -> String
+stringifyList xs = L.intercalate "\n" (map show xs)
+
+renderExpr :: CSBlock -> SourceBlock -> Html
+renderExpr uexpr block =
+  let uexpr' = addContextIds uexpr in
+  let ctxs = gatherContexts uexpr' in
+  toHtml $ cdiv "code-block" $ trace (
+    "[SrcPosCtx]:\n" ++ show (sbOffset block) ++ "\n" ++ T.unpack (sbText block) ++ "\n" ++
+    stringifyList ctxs
+    ++ "\n" -- ++ renderHtml (spanDelimitedCode block ctxs)
+  )
+  spanDelimitedCode block ctxs
 
 highlightSyntax :: T.Text -> Html
 highlightSyntax s = foldMap (uncurry syntaxSpan) classified
@@ -99,11 +226,12 @@ syntaxSpan s c = H.span (toHtml s) ! class_ (stringValue className)
       SymbolStr   -> "symbol"
       TypeNameStr -> "type-name"
       IsoSugarStr -> "iso-sugar"
+      WhitespaceStr -> "whitespace"
       NormalStr -> error "Should have been matched already"
 
 data StrClass = NormalStr
               | CommentStr | KeywordStr | CommandStr | SymbolStr | TypeNameStr
-              | IsoSugarStr
+              | IsoSugarStr | WhitespaceStr
 
 classify :: Parser StrClass
 classify =
@@ -115,6 +243,7 @@ classify =
    <|> try (char '#' >> (char '?' <|> char '&' <|> char '|' <|> pure ' ')
         >> lowerWord >> return IsoSugarStr)
    <|> (some symChar >> return SymbolStr)
+   <|> (some space1 >> return WhitespaceStr)
    <|> (anySingle >> return NormalStr)
 
 lowerWord :: Parser String